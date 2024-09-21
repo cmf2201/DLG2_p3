9d7f7b1ff634d98b1d7252f02a7fb7cf060d07ae
Caleb <cmfrench@wpi.edu>
2024-09-21 16:16:57 -0400

    Mask created

--- src/create_mask.py
+++ src/create_mask.py
@@ -1,6 +1,5 @@
 import torch
 import argparse
-import cv2
 import numpy as np
 import time
 import os
@@ -11,15 +10,19 @@ import torch
 import torchvision 
 from torchvision.io import read_image 
 import torchvision.transforms as T 
+to_image = T.ToPILImage()
+img = torchvision.io.read_image("/home/cmfrench/RBE474X/DeepLearningPart2/DLG2_p3/src/baseline_patch.png")
 
-img = torchvision.io.read_image('baseline_patch.png')
-print(img)
 
 for i in range(img.size(0)):
     for k in range(img.size(1)):
         for l in range(img.size(2)):
-            if(img[k-112]**2 + img[l-112]**2 > 112**2):
+            if((k-(img.size(1)/2))**2 + (l-(img.size(2)/2))**2 > (img.size(1)/2)**2):
                 img[i, k, l] = 0
+            else:
+                img[i, k, l] = 255
 
-print(img)
+image = to_image(img)
+image.convert("RGB")
+image.save("/home/cmfrench/RBE474X/DeepLearningPart2/DLG2_p3/src/mask.png")
 
