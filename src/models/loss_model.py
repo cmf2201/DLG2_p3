b8a828ad514364c8b2eeb843337943558bc79ac0
Chaddyoh <ctnguyen@wpi.edu>
2024-09-30 19:22:20 -0400

    made tv_loss

--- src/models/loss_model.py
+++ src/models/loss_model.py
@@ -1,6 +1,8 @@
 import torch
 import os
 from torch import nn
+import torchvision
+import torchvision.transforms.functional
 
 class AdversarialLoss(nn.Module):
     def __init__(self, args):
@@ -51,12 +53,25 @@ class AdversarialLoss(nn.Module):
         min_distances_final = min_distances.view(new_patch.shape[0], new_patch.shape[1], 1).repeat(1, 1, 3) # reshapes back to (H, W, C)
         loss = min_distances_final.permute(0, 1, 2) # back to (C, H, W)
         target_tensor = torch.zeros_like(self.patch).requires_grad_().cuda() # assuming target minimum distance is 0
-        return nn.L1loss(reduction='sum')(loss, target_tensor)
+        return nn.L1Loss(reduction='mean')(loss, target_tensor)
          
     
     # Total Variation Loss TODO: Implement
     def calc_tv_loss(self):
-        return self.patch
+        # assuming patch is 3*H*W
+        patch = self.patch
+        patch_compare_x = self.patch
+        patch_compare_y = self.patch
+
+        patch_compare_x = nn.functional.pad(patch_compare_x, (1,0,0,0), "constant", 0)
+        patch_compare_x = torchvision.transforms.functional.crop(patch_compare_x, 0, 0, patch.size(dim=2), patch.size(dim=1))
+
+        patch_compare_y = nn.functional.pad(patch_compare_y, (0,0,1,0), "constant", 0)
+        patch_compare_y = torchvision.transforms.functional.crop(patch_compare_y, 0, 0, patch.size(dim=2), patch.size(dim=1))
+
+        tensor_of_error = torch.sqrt(torch.square(patch - patch_compare_x) + torch.square(patch - patch_compare_y)).cuda()
+        tensor_targer = torch.zeros_like(tensor_of_error).requires_grad_().cuda()
+        return nn.L1Loss(reduction='mean')(tensor_of_error, tensor_targer)
     
     def calc_disp_loss(self):
         return nn.L1Loss(reduction='sum')(self.depth, (self.target))
